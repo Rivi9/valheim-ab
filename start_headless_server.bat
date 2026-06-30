115b7c6e9af588353c5c793e2be12b472387222b
m4theesha <im.matheesha@gmail.com>
2026-07-01 01:20:33 +0530

    hutto

---
+++ start_headless_server.bat
@@ -0,0 +1,9 @@
+@echo off
+set SteamAppId=892970
+
+echo "Starting server PRESS CTRL-C to exit"
+
+REM Tip: Make a local copy of this script to avoid it being overwritten by steam.
+REM NOTE: Minimum password length is 5 characters & Password cant be in the server name.
+REM NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
+valheim_server -nographics -batchmode -name "ARMORED BRIGADE" -port 2456 -world "AB" -crossplay -public 1 -password "AB4321" -saveinterval 1800 -backups 4 -backupshort 7200 -backuplong 43200
